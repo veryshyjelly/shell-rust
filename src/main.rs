4590c74690179f7a64597b50d5c3358237a05687
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2024-12-05 20:38:51 +0530

    quotes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use regex::Regex;
+use regex::RegexSet;
 use std::{
     env,
     io::{self, Write},
@@ -18,12 +19,25 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let mut command_split = input.trim().split(" ");
+        // let re =
+        //     RegexSet::new(&[r#"(?<arg>\w+)"#, r#"'(?<arg>.*?)'"#, r#""(?<arg>.*?)""#]).unwrap();
 
-        let command = command_split.next().unwrap();
-        let args: Vec<&str> = command_split.collect();
+        let re = Regex::new(r#"(?<arg1>\w+)|'(?<arg2>.*?)'|"(?<arg3>.*?)""#).unwrap();
 
-        match command {
+        let mut command_split = re.captures_iter(input.trim());
+
+        let command = command_split.next().unwrap()["arg1"].to_string();
+        let args: Vec<String> = command_split
+            .map(|x| {
+                (String::new()
+                    + x.name("arg1").map_or("", |x| x.as_str())
+                    + x.name("arg2").map_or("", |x| x.as_str())
+                    + x.name("arg3").map_or("", |x| x.as_str()))
+                .to_string()
+            })
+            .collect();
+
+        match command.as_str() {
             "exit" => {
                 let code = args[0].parse().unwrap();
                 exit(code);
@@ -32,24 +46,14 @@ fn main() {
                 println!("{}", args.join(" "));
             }
             "type" => {
-                sh_type(args[0]);
+                sh_type(&args[0]);
             }
             "pwd" => {
                 let current_dir = env::current_dir().unwrap();
                 println!("{}", current_dir.display());
             }
             "cd" => {
-                let path = if args[0] == "~" {
-                    env::var("HOME").unwrap()
-                } else {
-                    args[0].to_string()
-                };
-                let path = Path::new(&path);
-                if path.exists() {
-                    env::set_current_dir(path).unwrap();
-                } else {
-                    println!("cd: {}: No such file or directory", args[0]);
-                }
+                cd(&args[0]);
             }
             comm => {
                 if let Some(path) = search_in_path(comm) {
@@ -62,15 +66,29 @@ fn main() {
     }
 }
 
-fn run_exec(path: PathBuf, args: Vec<&str>) {
+fn cd(arg: &String) {
+    let path = if arg == "~" {
+        env::var("HOME").unwrap()
+    } else {
+        arg.to_string()
+    };
+    let path = Path::new(&path);
+    if path.exists() {
+        env::set_current_dir(path).unwrap();
+    } else {
+        println!("cd: {}: No such file or directory", arg);
+    }
+}
+
+fn run_exec(path: PathBuf, args: Vec<String>) {
     let mut c = Command::new(path).args(args).spawn().unwrap();
     c.wait().unwrap();
 }
 
-fn sh_type(comm: &str) {
+fn sh_type(comm: &String) {
     let builtin_commads = ["exit", "echo", "type", "pwd"];
 
-    if builtin_commads.contains(&comm) {
+    if builtin_commads.contains(&comm.as_str()) {
         println!("{} is a shell builtin", comm);
         return;
     }
