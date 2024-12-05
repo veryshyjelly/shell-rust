ef6c095cd8e6505b4765757fd23932e676fbaf2c
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2024-12-05 16:27:55 +0530

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use regex::Regex;
+use std::{
+    io::{self, Write},
+    process::exit,
+};
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -12,6 +16,15 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        println!("{}: command not found", input.trim());
+        let command = input.trim();
+
+        let re_exit = Regex::new(r"exit (?<code>\w+)").unwrap();
+
+        if let Some(code) = re_exit.captures(command) {
+            let code_point: i32 = code["code"].parse().unwrap();
+            exit(code_point);
+        } else {
+            println!("{}: command not found", command);
+        }
     }
 }
